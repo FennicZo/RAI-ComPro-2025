290674751f219bbd463255ba37ab5cb1e38a0200
FennicZoTH <97013152+FennicZo@users.noreply.github.com>
2025-07-18 03:26:09 +0000

    Added ex01.c

---
+++ ex01.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("Salary          :%8d\n",6500);
+    printf("Money per Week  :%8.2f\n",6500/4.0);
+    printf("Money per Day   :%8.3f\n",6500/4.0/7.0);
+    
+    return 0;
+}
