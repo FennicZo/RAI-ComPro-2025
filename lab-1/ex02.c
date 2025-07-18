352098039eb5d5f57f7b70212f5db88c019c56bb
FennicZoTH <97013152+FennicZo@users.noreply.github.com>
2025-07-18 03:54:09 +0000

    Editted ex02.c

--- lab-1/ex02.c
+++ lab-1/ex02.c
@@ -3,7 +3,7 @@
 int main() {
     printf("My name is %s\n","March");
     printf("I am %d years old.\n",18);
-    printf("I am %.1f cm tall.",171.5);
+    printf("I am %.1f cm tall",171.5);
     
     return 0;
 }
