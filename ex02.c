40ca1799a5490e3c606b0687d17928414e24d48d
FennicZoTH <97013152+FennicZo@users.noreply.github.com>
2025-07-18 03:33:18 +0000

    Added ex02.c

---
+++ ex02.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("Programming     :%3s\n","A");
+    printf("Drawing         :%4s\n","B+");
+    printf("Mechanics       :%4s\n","B+");
+    printf("Math            :%3s\n","B");
+    printf("GPS             :%5.1f\n",3.5);
+    
+    return 0;
+}
