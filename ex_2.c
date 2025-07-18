46f38ed7409866b1091024e2ed8ba6d010ce803f
FennicZoTH <97013152+FennicZo@users.noreply.github.com>
2025-07-18 02:57:45 +0000

    Editted ex_2.c

--- ex_2.c
+++ ex_2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    printf ("My name is : %s\n","Kmitl");
+    printf ("My name is : %s\n","March");
     printf ("My point : %d\n",20+30+39);
     printf ("Grade : %c\n", 'A');
     printf ("GPA : %f\n",3.99);
