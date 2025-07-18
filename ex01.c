01d30bc4708b808420039562fcea8db950a59469
FennicZoTH <97013152+FennicZo@users.noreply.github.com>
2025-07-18 03:43:36 +0000

    Added ex01.c

--- ex01.c
+++ ex01.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 
 int main() {
-
-    printf("Salary          :%9d\n",6500);
-    printf("Money per Week  :%9.2f\n",6500/4.0);
-    printf("Money per Day   :%9.3f\n",6500/4.0/7.0);
+    printf("Hello, World! \n");
+    printf("Welcome to RAI");
     
     return 0;
 }
