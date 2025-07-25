ca4526795e476726cbb6e83dcc95a2767a4b8f96
FennicZoTH <97013152+FennicZo@users.noreply.github.com>
2025-07-25 01:10:40 +0000

    Removed ex_1.c, ex_2.c, ex_3.c

--- ex_1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("Hello, World! \n");
-    printf ("This is my first Program. \n");
-    printf ("\tI\'m an engineer.");
-
-    return 0;
-}
--- ex_3.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-int main() {
-    printf ("Age = %d, GPA = %.2f\n",19,3.5);
-    printf ("Programming: %.2f\nPhysics: %.2f\n",4.0,3.75);
-    
-    return 0;
-}
