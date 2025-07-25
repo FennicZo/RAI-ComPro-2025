efc7804ea9306d7ab2e80e1ef4f8c6ac83bebc2a
FennicZoTH <97013152+FennicZo@users.noreply.github.com>
2025-07-25 03:51:31 +0000

    Editted ex05.c

--- lab-2/ex05.c
+++ lab-2/ex05.c
@@ -3,29 +3,36 @@
 
 int main() {
     char fullName[100];
+    char firstName[50], lastName[50];
     int age;
     float height;
     char university[100];
 
-    
     printf("Enter your full name: ");
     fgets(fullName, sizeof(fullName), stdin);
+    size_t len = strlen(fullName);
+    if (fullName[len - 1] == '\n') {
+        fullName[len - 1] = '\0';
+    }
+
+    sscanf(fullName, "%s %s", firstName, lastName);
+
     printf("Enter your age: ");
     scanf("%d", &age);
+
     printf("Enter your height: ");
     scanf("%f", &height);
-    getchar();
+    while (getchar() != '\n');
+
     printf("Enter your University name: ");
     fgets(university, sizeof(university), stdin);
-
-    if (fullName[strlen(fullName) - 1] == '\n') {
-        fullName[strlen(fullName) - 1] = '\0';
-    }
-    if (university[strlen(university) - 1] == '\n') {
-        university[strlen(university) - 1] = '\0';
+    len = strlen(university);
+    if (university[len - 1] == '\n') {
+        university[len - 1] = '\0';
     }
 
-    printf("\nHi! Everyone. This is %s from %s. I am %d years old and my height is\n%.1f cm tall.\n", fullName, university, age, height);
+    printf("\nHi! Everyone. This is %c.%s from %s. I am %d years old and my height is %.1f cm tall.\n",
+           lastName[0], firstName, university, age, height);
 
     return 0;
 }
