9d9c7b3e8f53d4cd281cdfc090ec02361eb60b5b
FennicZoTH <97013152+FennicZo@users.noreply.github.com>
2025-08-06 02:50:43 +0000

    Added lab-3

---
+++ lab-3/ex02.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int number;
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    if (number >= 1 && number <= 100) {
+        if (number % 2 == 0) {
+            printf("%d is even\n", number);
+        } else {
+            printf("%d is odd\n", number);
+        }
+    } 
+    else {
+
+        printf("%d is out of range\n", number);
+    }
+
+    return 0;
+}
