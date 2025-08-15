cc4c5440cabd5fb0b665a3b46763503315500587
FennicZoTH <97013152+FennicZo@users.noreply.github.com>
2025-08-15 02:58:55 +0000

    Editted ex01.c

--- lab-5/ex01.c
+++ lab-5/ex01.c
@@ -10,12 +10,14 @@ int main() {
     }
     
     printf("Values in array are: ");
+    
     for (int i = 0; i < 10; i++) {
         printf("%d", values[i]);
         if (i < 9) {
             printf(", ");
         }
     }
+    
     printf("\n");
     
     return 0;
