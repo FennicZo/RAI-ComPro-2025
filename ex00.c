7e50e57b26ced7a85b22599d3d3ea5c455642f19
FennicZoTH <97013152+FennicZo@users.noreply.github.com>
2025-09-05 04:05:28 +0000

    Editted a.out & ex00.c

--- ex00.c
+++ ex00.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 
 int main() {
-
-    int num, i, j;
-
-    printf("Enter the size of the square: ");
-    scanf("%d", &num);
-
-    for (i = 1; i <= num; i++) {
-        printf("\n");
-        for (j = 1; j <= num; j++) {
-            if (i == 1 || i == num || j == 1 || j == num)
-                printf("*");
-            else
-                printf(" ");
-        }
-    }
-
-    printf("\n");
-    return 0;
+    struct book {
+        char name[50];
+        float price;
+        float discount;
+    } book1;
     
+    printf ("Enter book name : ");
+    scanf ("%s",book1.name);
+    printf ("Enter book price : ");
+    scanf ("%f",&book1.price);
+    
+    book1.discount = 0.1*book1.price;
+    printf ("\n\n\n\nBook : %s\n",book1.name);
+    printf ("Price : %.2f\n",book1.price);
+    printf ("Discount 10 percent : %.2f\n"
+
+    "Total price : %.2f\n",
+    book1.discount,
+    book1.price - book1.discount);
+
+    return 0;
 }
