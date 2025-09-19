4fbe259cfa6237341b03201b096734e2be032968
FennicZoTH <97013152+FennicZo@users.noreply.github.com>
2025-09-19 02:54:23 +0000

    Added ex05.c

---
+++ lab-8/ex05.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void sumEvenOdd(int arr[], int n, int *evenSum, int *oddSum) {
+    *evenSum = 0;
+    *oddSum = 0;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] % 2 == 0) {
+            *evenSum += arr[i];
+        } else {
+            *oddSum += arr[i];
+        }
+    }
+}
+
+int main() {
+    int n;
+
+    printf("N: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        printf("Input: ");
+        scanf("%d", &arr[i]);
+    }
+
+    int evenSum, oddSum;
+    sumEvenOdd(arr, n, &evenSum, &oddSum);
+
+    printf("Output: Sum of even number: %d\n", evenSum);
+    printf("        Sum of odd number: %d\n", oddSum);
+
+    return 0;
+}
