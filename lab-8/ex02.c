55c06b8e108639974d9a268392878fe998bba80f
FennicZoTH <97013152+FennicZo@users.noreply.github.com>
2025-09-19 02:09:50 +0000

    Added ex02.c

---
+++ lab-8/ex02.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int isPrime(int num) {
+    if (num <= 1) {
+        return 0;
+    }
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void displayPrimes(int start, int end) {
+    printf("\nThe prime numbers within the intervals are:\n");
+    for (int i = start; i <= end; i++) {
+        if (isPrime(i)) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+}
+
+int main() {
+    int start, end;
+
+    printf("Enter the start and end numbers: ");
+    scanf("%d %d", &start, &end);
+
+    displayPrimes(start, end);
+
+    return 0;
+}
